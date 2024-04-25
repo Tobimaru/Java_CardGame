0834bb794378ac7cbd622cc0c03f3cb8ba276f3f
Tobimaru <honfai.choi@gmail.com>
2024-04-25 19:01:33 +0200

    use static initilization in BlackJack

--- cards/src/main/java/com/github/Tobimaru/cards/BlackJack.java
+++ cards/src/main/java/com/github/Tobimaru/cards/BlackJack.java
@@ -55,60 +55,57 @@ public class BlackJack extends CardGame {
     }
     
     private static final HashMap<Rank,BJValue> BJ_CARD_VALUES = new HashMap<Rank,BJValue>();
-    
-    private static void assign_card_values(){
-       if (BJ_CARD_VALUES.isEmpty()){
-            for (Rank rk : Rank.values()){
-                BJValue a_val = new BJValue();
+    static {
+        for (Rank rk : Rank.values()){
+            BJValue a_val = new BJValue();
+            
+            switch (rk){
+                case ACE: {
+                    a_val.soft = 1;
+                    a_val.hard = 11; 
+                    break;}
+                case TWO: {
+                    a_val.soft = 2;
+                    a_val.hard = 2;
+                    break;}
+                case THREE: {
+                    a_val.soft = 3;
+                    a_val.hard = 3;
+                    break;}
+                case FOUR: {
+                    a_val.soft = 4;
+                    a_val.hard = 4;
+                    break;}
+                case FIVE: {
+                    a_val.soft = 5;
+                    a_val.hard = 5;
+                    break;}
+                case SIX: {
+                    a_val.soft = 6;
+                    a_val.hard = 6;
+                    break;}
+                case SEVEN: {
+                    a_val.soft = 7;
+                    a_val.hard = 7;
+                    break;}
+                case EIGHT: {
+                    a_val.soft = 8;
+                    a_val.hard = 8;
+                    break;}
+                case NINE: {
+                    a_val.soft = 9;
+                    a_val.hard = 9;
+                    break;}        
+                case TEN: 
+                case JACK:
+                case QUEEN:
+                case KING: a_val.soft = 10;
+                            a_val.hard = 10;
+                            break; 
+            }
                 
-                switch (rk){
-                    case ACE: {
-                        a_val.soft = 1;
-                        a_val.hard = 11; 
-                        break;}
-                    case TWO: {
-                        a_val.soft = 2;
-                        a_val.hard = 2;
-                        break;}
-                    case THREE: {
-                        a_val.soft = 3;
-                        a_val.hard = 3;
-                        break;}
-                    case FOUR: {
-                        a_val.soft = 4;
-                        a_val.hard = 4;
-                        break;}
-                    case FIVE: {
-                        a_val.soft = 5;
-                        a_val.hard = 5;
-                        break;}
-                    case SIX: {
-                        a_val.soft = 6;
-                        a_val.hard = 6;
-                        break;}
-                    case SEVEN: {
-                        a_val.soft = 7;
-                        a_val.hard = 7;
-                        break;}
-                    case EIGHT: {
-                        a_val.soft = 8;
-                        a_val.hard = 8;
-                        break;}
-                    case NINE: {
-                        a_val.soft = 9;
-                        a_val.hard = 9;
-                        break;}        
-                    case TEN: 
-                    case JACK:
-                    case QUEEN:
-                    case KING: a_val.soft = 10;
-                               a_val.hard = 10;
-                               break; 
-                }
-                
-                BJ_CARD_VALUES.put(rk, a_val);
-            } 
-       }
+            BJ_CARD_VALUES.put(rk, a_val);
+        } 
     }
     
     private class BJHand{
@@ -235,8 +232,6 @@ public class BlackJack extends CardGame {
     public BlackJack(int nmb_decks){
         
         super(nmb_decks); //calling parent constructor CardGame(nmb_decks)
-        
-        assign_card_values();
         
         this.dealer_hand = new BJHand();
         
